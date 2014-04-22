bbb2d4b4a0f2521fb2f89669dd5fb44cfb718530
dzenbot <iromero@dzen.cl>
2014-04-22 17:46:51 -0300

    Renames annotation class

---
+++ Source/DZNAnnotation.h
@@ -0,0 +1,22 @@
+//
+//  DZNMapViewAnnotation.h
+//  DZNMapViewController
+//
+//  Created by Ignacio Romero Zurbuchen on 10/7/13.
+//  Copyright (c) 2013 DZN Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import "DZNLocation.h"
+
+@interface DZNAnnotation : NSObject <MKAnnotation>
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, assign) DZNLocation *location;
+
+- (id)initWithTitle:(NSString *)title subtitle:(NSString *)subtitle andCoordinate:(CLLocationCoordinate2D)coordinate;
+
+@end
